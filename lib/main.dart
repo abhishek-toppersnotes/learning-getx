42722852ef0400553da4fcf01edcd09302e0ec99
abhishek-toppersnotes <abhishek@toppersnotes.com>
2022-01-03 23:39:42 +0530

    Initial commit with just the bare-bones structure of the code.

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String title = "Bare-bones";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+      ),
+    );
+  }
+}
