abb2ba83c0859c7248923d575de70123b6e88c48
abhishek-toppersnotes <abhishek@toppersnotes.com>
2022-01-05 01:01:08 +0530

    Simple state mgmt with GetBuilder() builder func

--- lib/increment_controller.dart
+++ lib/increment_controller.dart
@@ -2,9 +2,13 @@ import 'package:get/get.dart';
 
 // We can define a getxController to seperate the business logic from the ui logic
 class IncrementController extends GetxController {
-  RxInt counterValue = 0.obs;
+  // Unlike reactive state management, we can use normal variables in Simple State mgmt
+  // These values are to be updated using update() function which GetxController provides
+  // and shall ONLY BE USED by GetBuilder() builder method
+  int counterValue = 0;
 
   void increment() {
-    counterValue.value++;
+    counterValue++;
+    update();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
   static const String title =
-      "Reactive state mgmt using GetX() Builder function";
+      "Simple state mgmt using GetBuilder() builder function";
 
   // ? We can't use 'static' here
   // ? Need to ask why that is so?
   // ? I have used static controller in lecture9 as well, and there it works as expected
-  final IncrementController incrementController = Get.put(
-      IncrementController()); // ! NOT just IncrementController(), must use Get.put()
+  // final IncrementController incrementController = Get.put(
+  //     IncrementController()); // ! NOT just IncrementController(), must use Get.put()
 
   @override
   Widget build(BuildContext context) {
@@ -33,24 +33,21 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              GetX<IncrementController>(
+              GetBuilder<IncrementController>(
                 // If we don't define [init] of GetX, we must have a controller initialized to use the value
                 // Otherwise, it will throw error
                 //
                 // If we have initialized the controller else where, we can
-                // init: IncrementController(),
+                init: IncrementController(),
 
                 builder: (controller) {
                   // If we are using [init]
-                  // return Text(
-                  //     "Current Value: ${controller.counterValue.value}");
+                  return Text("Current Value: ${controller.counterValue}");
 
                   // If we are having an instance defined somewhere else, [currentValue] received
                   // is not even required
-                  return Text(
-                      "Current Value: ${incrementController.counterValue.value}");
-
-                  // return Container();
+                  // return Text(
+                  //     "Current Value: ${incrementController.counterValue}");
                 },
               ),
               SizedBox(
@@ -62,10 +59,10 @@ class MyApp extends StatelessWidget {
                   // then we must first find the instance of that controller
                   //
                   // Use this only if we don't have the instance available here
-                  // Get.find<IncrementController>().increment();
+                  Get.find<IncrementController>().increment();
 
-                  // If we have access to the [IncrementController] direcly
-                  incrementController.increment();
+                  // If we have access to the [IncrementController] direcly (like if we have initialized it)
+                  // incrementController.increment();
                 },
                 child: Text("Click Me"),
               ),
